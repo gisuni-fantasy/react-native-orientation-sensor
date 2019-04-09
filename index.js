604b403d19c5a45bf1a4577cb2d1dd578e1ee3d8
695954085 <695954085@qq.com>
2019-04-09 16:51:56 +0800

    feat: add js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-
-import { NativeModules } from 'react-native';
-
+import { NativeModules, DeviceEventEmitter } from "react-native";
+import { Observable } from "rxjs";
+import { refCount, publish} from 'rxjs/operators'
 const { RNOrientation } = NativeModules;
 
-export default RNOrientation;
+function createSensorObservable() {
+  return Observable.create(function subscribe(observer) {
+    this.isSensorAvailable = false;
+
+    this.unsubscribeCallback = () => {
+      if (!this.isSensorAvailable) return;
+      RNOrientation.stopUpdates();
+    };
+
+    RNOrientation.isAvailable().then(
+      () => {
+        DeviceEventEmitter.addListener("RNOrientation", data => {
+          observer.next(data);
+        });
+
+        this.isSensorAvailable = true;
+
+        RNOrientation.startUpdates();
+      },
+      () => {
+        observer.error(`Orientation sensor is not available`);
+      }
+    );
+
+    return this.unsubscribeCallback;
+  }).pipe(publish(), refCount());
+}
+
+export default createSensorObservable()
+
+// export default RNOrientation;
